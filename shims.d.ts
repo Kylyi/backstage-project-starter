54dc5d5caa1f69efeccbf9a21aa7b44c18ea2637
Kylian, Jakub <jacob.kylian@gmail.com>
2024-09-22 19:58:30 +0200

    fix: type

--- shims.d.ts
+++ shims.d.ts
@@ -1,19 +1,6 @@
 /// <reference types="vite/client" />
 
-import type { IQueryBuilderRow } from '~/components/QueryBuilder/types/query-builder-row-props.type'
 import type { IAuthJwt } from '~/libs/Auth/types/auth-jwt.type'
-import type { prisma } from '~/server/prisma'
-
-export type IBoardLayoutSchema = {
-  columnsSelection?: number[]
-  tagsFilter?: number[]
-  usersFilter?: number[]
-  tasksQueryBuilder?: IQueryBuilderRow[]
-  taskFields?: string[]
-  useAdvancedFilter?: boolean
-  useUsersFilter?: boolean
-  useTagsFilter?: boolean
-}
 
 type Without<T, U> = {
   [P in Exclude<keyof T, keyof U>]?: never;
@@ -51,25 +38,6 @@ export type NavigateToOptions = {
   redirectCode?: number;
   external?: boolean;
   open?: OpenOptions;
-}
-
-declare module 'h3' {
-  interface H3EventContext {
-    auth?: IAuthJwt | null
-    instanceId?: number
-    prisma: typeof prisma
-  }
-}
-
-// Prisma JSON types
-declare global {
-  namespace PrismaJson {
-    type IStyle = IItem
-    type JsonValue = IItem
-    type IBoardLayoutSchema = IBoardLayoutSchema
-    type IWorkflowCondition = IItem
-    type IWorkflowAction = IItem
-  }
 }
 
 export default {}
